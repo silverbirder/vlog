8fc44c8832e5169c2c9e6d808536e5dfb99fa77a
shibamoto <silverbirder@gmail.com>
2025-09-22 21:15:40 +0900

    feat: add Test component and update main entry point to use it

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./pages";
+import { Test } from "./pages";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Test />
   </React.StrictMode>,
 );
